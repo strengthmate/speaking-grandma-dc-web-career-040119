545c9dadb781b34b4f71917bbf72b09afec7c5e2
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-15 16:45:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@
 def speak_to_grandma (phrase)
   if phrase == phrase.downcase
     return 'HUH?! SPEAK UP, SONNY!'
-  else if phrase == 'I LOVE YOU GRANDMA!' && phrase.upcase
+  else if phrase == 'I LOVE YOU GRANDMA!' && phrase.upcase()
     return 'I LOVE YOU TOO PUMPKIN!'
  else
     return 'NO, NOT SINCE 1938!'
